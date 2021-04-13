e4869d22ab9257afe4cfd6b1db6685f0f80f1771
siddgrg <sidgurung97@gmail.com>
2021-04-13 12:18:50 +0100

    location toggle styles, locations array

--- src/App.js
+++ src/App.js
@@ -8,12 +8,31 @@ import { API_KEY, URL } from './CONSTS.json';
 
 function App() {
 
-  const [location, setLocation] = useState('London');
-  const [latitude, setLatitude] = useState(51.5074);
-  const [longitude, setLongitude] = useState(0.1278);
+  const locations = [
+    ['London', 51.5074, -0.1278],
+    ['New Delhi', 28.7041, 77.1025],
+    ['Tokyo', 35.6762, 139.6503],
+    ['Shanghai', 31.2304, 121.4737],
+    ['São Paulo', -23.533773, -46.625290],
+    ['Mexico City', 19.4326, -99.1332],
+    ['New York', 40.7128, -74.0060],
+    ['Istanbul', 41.0082, 28.9784],
+    ['Cairo', 30.0444, 31.2357],
+    ['Pokhara', 28.2096, 83.9856]
+  ]
+
+  const [location, setLocation] = useState(locations[0][0]);
+  const [latitude, setLatitude] = useState(locations[0][1]);
+  const [longitude, setLongitude] = useState(locations[0][2]);
   const [weatherData, setWeatherData] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [error, setError] = useState(null);
+
+  const setLocationHandle = (index) => {
+    setLatitude(locations[index][1]);
+    setLongitude(locations[index][2]);
+    setLocation(locations[index][0]);
+  }
 
   useEffect(() => {
     setLoaded(false);
@@ -28,13 +47,17 @@ function App() {
         setLoaded(true);
         console.log(error);
       })
-  }, [location])
+  }, [location, latitude, longitude])
 
   return (
     <div class="container mt-5">
       <div class="d-flex flex-column align-items-center">
         <Header />
-        <LocationSearch />
+        <LocationSearch
+          currentLocation={location}
+          locations={locations.map((l) => l[0])}
+          setLocationHandle={setLocationHandle}
+        />
         {loaded && error === null && <TabbedCards weatherData={weatherData} />}
         {loaded && error && <p>There was an error loading the weather</p>}
         {!loaded && <p>Loading...</p>}
--- src/component/Search/LocationSearch.jsx
+++ src/component/Search/LocationSearch.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 
-const LocationSearch = () => {
-    return <span id="temp-city">London</span>
+const LocationSearch = ({ currentLocation, locations, setLocationHandle }) => {
+
+    const [toggleTitle, setToggleTitle] = useState(currentLocation);
+
+    const LocationToggle = React.forwardRef(({ children, onClick }, ref) => (
+        <a
+            href=""
+            ref={ref}
+            onClick={(e) => {
+                e.preventDefault();
+                onClick(e);
+            }}
+            className="locationToggle"
+        >
+            <span>{children}</span>
+        </a>
+    ));
+
+    const locationDropdownItems = locations.map((l, i) => <Dropdown.Item key={i} eventKey={i}>{l}</Dropdown.Item>)
+
+    const onSelectLocationHandle = (eventKey, e) => {
+        setToggleTitle(e.target.text);
+        setLocationHandle(eventKey);
+    }
+
+    return (
+        <>
+            <Dropdown className="locationDropdown" onSelect={onSelectLocationHandle}>
+                <Dropdown.Toggle as={LocationToggle} id="location-toggle">{toggleTitle}</Dropdown.Toggle>
+                <Dropdown.Menu>
+                    {locationDropdownItems}
+                </Dropdown.Menu>
+            </Dropdown>
+        </>
+    )
 }
 
 export default LocationSearch;
