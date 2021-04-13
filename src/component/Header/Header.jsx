b98023450f003965a34679c2da257f9ab41c1536
siddgrg <sidgurung97@gmail.com>
2021-04-13 12:34:52 +0100

    header bounce animation

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Header = () => {
-    return <h1>WEATHER-REACT</h1>
+    return <h1><a href="https://github.com/siddgrg/weather-react" target="_blank" rel="noreferrer">WEATHER-REACT</a></h1>
 }
 
 export default Header;
